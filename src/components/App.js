edb27c28bed1bdce5102ba9b4e2329a382f1622c
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-07-26 20:26:54 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
+      {/* <header>
         <h2>Shopster</h2>
         <button onClick={handleDarkModeClick}>
           {isDarkMode ? "Dark" : "Light"} Mode
         </button>
-      </header>
+      </header> */}
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick}/>
       <ShoppingList items={itemData} />
     </div>
   );
 }
-
 export default App;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,28 +1,26 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from "./Filter";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
-
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
-
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
   });
-
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
+        {/* <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
-        </select>
+        </select> */}
+        <Filter selectedCategory={selectedCategory} onCategoryChange={handleCategoryChange}/>
       </div>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
@@ -32,5 +30,4 @@ function ShoppingList({ items }) {
     </div>
   );
 }
-
 export default ShoppingList;
